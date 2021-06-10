1d2617dd6a0edd0dabc3d37e84a9fa3f92a3a2e6
yj <liuwu4@yeah.net>
2021-06-10 10:41:46 +0800

    chore：规则校验

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: yj
  * @Date: 2021-05-24 15:41:15
- * @LastEditTime: 2021-05-27 17:19:53
+ * @LastEditTime: 2021-06-09 10:05:15
  * @Description: vue 配置文件
  */
 const path = require('path');
@@ -9,7 +9,7 @@ module.exports = {
   runtimeCompiler: true,
   configureWebpack: {
     resolve: {
-      extensions: ['.js', '.vue', 'json'],
+      extensions: ['.js', '.vue', '.json'],
       alias: {
         '@': path.resolve(__dirname, './src')
       }
